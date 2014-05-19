30497b7e0082358f19e05fa076d4f9d77215303e
Eirik Vullum <evullum@gmail.com>
2014-05-19 10:50:31 +0200

    refactor for multiple instances

--- example/test.js
+++ example/test.js
@@ -15,15 +15,19 @@ var handleEntry = function (item, callback) {
 var getSites = function (options, callback) {
     var sites = require('./template');
     callback(null, sites.filter(isActive));
-}
+};
+
+// SiteScraper
+var SiteScraper = require('../lib')();
 
 // website scraper module
-var siteScraper = require('../lib/')({
+var siteScraper = new SiteScraper({
     getSources: getSites,
     handleEntry: handleEntry,
     ipc: ipc,
     sockets: 15, // experiment with this number to avoid getting blocked connections (DDOS protection)
-    waitTime: 10000
+    waitTime: 10000,
+    timeOut: 5000
 });
 
 console.log('running scraper');
